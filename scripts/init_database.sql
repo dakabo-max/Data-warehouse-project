4a67a92d0055ebcac8c2d2a07b99f25ad1a9cebd
Jonathan Lartey Teye <jonathanlarteydakabo@gmail.com>
2025-05-23 14:05:58 +0100

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,18 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+*/
+-- Database
+CREATE DATABASE DataWarehouse;
+
+-- Schemas
+CREATE SCHEMA silver;
+
+CREATE SCHEMA bronze;
+
+CREATE SCHEMA gold;
